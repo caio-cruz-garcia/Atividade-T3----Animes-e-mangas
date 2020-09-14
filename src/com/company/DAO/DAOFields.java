00733553b31eefdbc7fb8958d854acfc3bcbac36
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-14 18:37:13 -0300

    class DAOFields.java updated and converted to interface

--- src/com/company/DAO/DAOFields.java
+++ src/com/company/DAO/DAOFields.java
@@ -1,4 +1,18 @@
 package com.company.DAO;
 
-public class DAOFields {
+/**
+ * <h1>Interface DAOFields</h1>
+ * Like interface DAO, it contains the established method names for each class.
+ * <p>
+ * @version 1.0
+ * @since 2020-09-14
+ */
+
+public interface DAOFields {
+    String getTableName();
+    String getDeleteString(String table);
+    String getUpdateString(String table);
+    String getInsertString(String table);
+    String getSelectAllString(String table);
+    String getSelectConditionalString(String table);
 }
