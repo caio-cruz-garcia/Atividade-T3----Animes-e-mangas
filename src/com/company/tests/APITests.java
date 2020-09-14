ee79b4d8683d62bb8744707eb6bf39bd418535c3
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-14 20:42:27 -0300

    class APITests.java created

---
+++ src/com/company/tests/APITests.java
@@ -0,0 +1,25 @@
+package com.company.tests;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class APITests {
+    public static String apiTest(String request_url) throws Exception{
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .connectTimeout(Duration.ofSeconds(10))
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET().uri(URI.create(request_url)).build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+//        System.out.println("Status Code:" + response.statusCode());
+//        System.out.println("Body:" + response.body());
+        return response.body();
+    }
+}
