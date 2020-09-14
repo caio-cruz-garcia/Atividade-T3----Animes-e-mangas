db4faf089365fcb031f2d4092fe8c791f840bd11
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-14 17:35:12 -0300

    class Anime.java created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ package com.company;
  * <h1>class Main</h1>
  * Used to run the class "Execute", due to pdf request.
  * @author Nathan Brito da Silva 17.00531-0
- * @author 
+ * @author
  */
 
 public class Main {
