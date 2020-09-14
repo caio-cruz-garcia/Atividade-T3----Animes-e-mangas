09ad44f2be4126fa77663b5f03dfbdaedf72f968
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-14 18:36:24 -0300

    Interface DAO.java updated - V1

--- src/com/company/DAO/DAO.java
+++ src/com/company/DAO/DAO.java
@@ -1,4 +1,18 @@
 package com.company.DAO;
+import java.util.List;
 
-public interface DAO {
+/**
+ * <h1>Interface DAO</h1>
+ * Interface to be the basis of how sql commands are to be made.
+ * <p>
+ * @version 1.0
+ * @since 2020-09-14
+ */
+
+public interface DAO<T>{
+    List<T> get(String condition);
+    List<T> getAll();
+    void update(T t);
+    void delete(T t);
+    void create(T t);
 }
