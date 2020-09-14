ccf391d8f201ec10b3807a336631d640c05cbe14
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-14 17:21:45 -0300

    Project organization

---
+++ README.md
@@ -0,0 +1,2 @@
+Atividade T3 de programação.
+Informações necessárias no pdf "Atividade T3.pdf"
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * <h1>class Main</h1>
+ * Used to run the class "Execute", due to pdf request.
+ * @author Nathan Brito da Silva 17.00531-0
+ * @author 
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
