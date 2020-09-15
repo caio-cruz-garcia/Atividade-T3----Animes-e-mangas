f69854439ffbfdf52a332f3b3abbf3462c04a7dc
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-15 12:54:11 -0300

    interface CRUDFields.java updated

--- src/com/company/API/CRUDFields.java
+++ src/com/company/API/CRUDFields.java
@@ -1,16 +1,24 @@
 package com.company.API;
 
+import java.util.List;
+
 /**
  * <h1>Interface CRUD</h1>
  * Interface to be the basis of how sql commands are to be made.
  * <p>
  * @version 1.0
  * @since 2020-09-14
+ *
+ * Version 1.1
+ *      - Changed getByName from String to List<Anime>
+ *      - Changed getByGenre from String to List<Anime>
+ *      - Changed getByType from String to List<Anime>
+ *      - Modified interface to be <T>
  */
 
-public interface CRUDFields {
+public interface CRUDFields<T> {
     String getLink();
-    String getByName(String request);
-    String getByGenre(String request);
-    String getByType(String request);
+    List<T> getByName(String name);
+    List<T> getByGenre(String name, String genre);
+    List<T> getByType(String name, String type);
 }
