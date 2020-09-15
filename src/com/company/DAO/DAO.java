a4a0615d7474ec5c1ad9a25ad198da7844aeef9d
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-15 10:27:51 -0300

    interface DAO.java updated

--- src/com/company/DAO/DAO.java
+++ src/com/company/DAO/DAO.java
@@ -5,14 +5,19 @@ import java.util.List;
  * <h1>Interface DAO</h1>
  * Interface to be the basis of how sql commands are to be made.
  * <p>
- * @version 1.0
+ * @version 1.1
  * @since 2020-09-14
+ *
+ * Version 1.1
+ *      - changed create to int
+ *      - changed update to int
+ *      - changed delete to int
  */
 
 public interface DAO<T>{
     List<T> get(String condition);
     List<T> getAll();
-    void update(T t);
-    void delete(T t);
-    void create(T t);
+    int update(T t);
+    int delete(T t);
+    int create(T t);
 }
