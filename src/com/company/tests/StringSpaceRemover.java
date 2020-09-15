7a1bbceee83c6d6f8463d7fcbd5f0734800b8e7c
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-15 20:29:22 -0300

    Class StringSpaceRemover.java updated and tested - Found the issue
    It was using "scanner.next()",  appropriate is "scanner.nextLine()"

--- src/com/company/tests/StringSpaceRemover.java
+++ src/com/company/tests/StringSpaceRemover.java
@@ -1,4 +1,18 @@
 package com.company.tests;
 
+import java.util.Scanner;
+
 public class StringSpaceRemover {
+    public static String spaceFixer(String text){
+        text = text.replaceAll("\\s","%20");
+        return text;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(spaceFixer("This is a test"));
+        Scanner scanner = new Scanner(System.in);
+        String test = scanner.nextLine();
+        System.out.println(test);
+        System.out.println(spaceFixer(test));
+    }
 }
