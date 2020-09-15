1339c9ec554f4203a0193cd5a64529e0e9b48333
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-14 21:20:35 -0300

    enum MangaTypes.java updated

--- src/com/company/enums/MangaTypes.java
+++ src/com/company/enums/MangaTypes.java
@@ -1,4 +1,22 @@
 package com.company.enums;
 
 public enum MangaTypes {
+    MANGA("manga"),
+    NOVEL("novel"),
+    ONESHOT("oneshot"),
+    DOUJIN("doujin"),
+    MANHWA("manhwa"),
+    MANHUA("manhua");
+
+
+
+    private String display;
+    MangaTypes(String display) {
+        this.display = display;
+    }
+
+    @Override
+    public String toString() {
+        return display;
+    }
 }
