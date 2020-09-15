3090a8b8a43e23bd5353ec4292eb08787f7d4082
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-15 09:23:21 -0300

    enum AnimeTypes.java updated

--- src/com/company/enums/AnimeTypes.java
+++ src/com/company/enums/AnimeTypes.java
@@ -1,4 +1,26 @@
 package com.company.enums;
 
+/**
+ * <h1>Enum AnimeTypes</h1>
+ * Known anime types. While not used for the Anime itself, it is used in the CRUD searches.
+ * @version 1.0
+ * @since 2020-09-14
+ */
 public enum AnimeTypes {
+    TV("TV"),
+    OVA("OVA"),
+    MOVIE("Movie"),
+    SPECIAL("Special"),
+    ONA("ONA"),
+    MUSIC("Music");
+
+    private String display;
+    AnimeTypes(String display) {
+        this.display = display;
+    }
+
+    @Override
+    public String toString() {
+        return display;
+    }
 }
