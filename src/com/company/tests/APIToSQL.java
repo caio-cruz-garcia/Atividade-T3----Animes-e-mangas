d98e8bc27e7c166db772bf64c4bbd5a766f8a27a
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-09-15 20:06:31 -0300

    class APITToSQL.java updated

--- src/com/company/tests/APIToSQL.java
+++ src/com/company/tests/APIToSQL.java
@@ -1,15 +1,18 @@
 package com.company.tests;
 
 import com.company.API.AnimeCRUD;
+import com.company.API.MangaCRUD;
 import com.company.DAO.AnimeDAO;
+import com.company.DAO.MangaDAO;
 import com.company.model.Anime;
 import com.company.model.AnimeList;
+import com.company.model.Manga;
 
 import java.util.List;
 import java.util.Scanner;
 
 /**
- * <h1>class APIToSQL</h1>
+ * <h1>class APIToSQL (TEST)</h1>
  * This here we'll simulate the attempt at gathering an API and inserting it on the SQL table.
  * Both animes and mangas
  */
@@ -22,9 +25,17 @@ public class APIToSQL {
     public static void main(String[] args) {
         String goodAnime = "hellgirl";
 
+        String goodManga = "berserk";
+
         AnimeDAO animeDAO = new AnimeDAO();
         List<Anime> animeList = (animeDAO.get("name LIKE '%" + goodAnime + "%'" ));
         AnimeCRUD animeCRUD = new AnimeCRUD();
+
+        MangaDAO mangaDAO = new MangaDAO();
+        List<Manga> mangaList = (mangaDAO.get("name LIKE '%" + goodManga + "%'" ));
+        MangaCRUD mangaCRUD = new MangaCRUD();
+
+        /*
         if (animeList.size() != 0 ){
             System.out.println("Found in database.");
             for (Anime anime: animeList) {
@@ -44,9 +55,30 @@ public class APIToSQL {
             //animeDAO.create(anime);
         }
 
+         */
+        if (mangaList.size() != 0 ){
+            System.out.println("Found in database.");
+            for (Manga manga: mangaList) {
+                System.out.println("--------------------------");
+                System.out.println("Manga.: " + manga.getName());
+                System.out.println("Image.: " + manga.getUrl());
+                System.out.println("Synopsis.: " + manga.getSynopsis());
+                System.out.println("Score.: " + manga.getScore());
+
+            }
+
+        } else {
+            System.out.println("Not found in database. Moving to API call");
+            mangaList = mangaCRUD.getByName(goodManga);
+            Manga manga = selectOne(mangaList);
+            manga.getAll();
+            mangaDAO.create(manga);
+        }
+
 
     }
 
+    /*
     public static Anime selectOne(List<Anime> animeList){
         int exit = 0;
         Scanner scanner = new Scanner(System.in);
@@ -60,6 +92,28 @@ public class APIToSQL {
             if (choice <= animeList.size()){
                 exit = 1;
                 return animeList.get(choice);
+            } else {
+                System.out.println("Please input a valid choice");
+            }
+        }
+        return null;
+    }
+
+     */
+
+    public static Manga selectOne(List<Manga> mangaList){
+        int exit = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please select one.:");
+        for (int i = 0; i < mangaList.size(); i++) {
+            System.out.println(i + " - " + mangaList.get(i).getName());
+        }
+
+        while (exit == 0){
+            int choice = Integer.parseInt(scanner.next());
+            if (choice <= mangaList.size()){
+                exit = 1;
+                return mangaList.get(choice);
             } else {
                 System.out.println("Please input a valid choice");
             }
